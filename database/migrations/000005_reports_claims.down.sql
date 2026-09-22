999b1090e0c33c46689c275479965b07158d8ca4
wuangmmo <wuangmmo@gmail.com>
2026-09-22 11:34:06 +0700

    V3: add phone reports and ownership claims

---
+++ database/migrations/000005_reports_claims.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS phone_claims;
+DROP TABLE IF EXISTS phone_reports;
+DROP TYPE IF EXISTS claim_status;
+DROP TYPE IF EXISTS moderation_status;
+DROP TYPE IF EXISTS report_reason;
