04e30a2c5772819ddba5ca5437a796bae53aa933
wuangmmo <wuangmmo@gmail.com>
2026-09-22 18:40:05 +0700

    feat(v11): add phone profile rollback migration

---
+++ database/migrations/000013_phone_profile_v11.down.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS phone_identity_disputes;
+DROP TABLE IF EXISTS phone_identity_suggestions;
+DROP TABLE IF EXISTS phone_profile_signals;
+DROP TYPE IF EXISTS identity_match_status;
