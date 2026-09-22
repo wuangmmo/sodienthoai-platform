62a75555050edf009be922e8c1ac8a3629f53ed0
wuangmmo <wuangmmo@gmail.com>
2026-09-22 21:39:43 +0700

    feat(v15): rollback community votes and reports

--- database/migrations/000017_accounts_community_alerts_v15.down.sql
+++ database/migrations/000017_accounts_community_alerts_v15.down.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS comment_reports;
+DROP TABLE IF EXISTS comment_helpful_votes;
 DROP TABLE IF EXISTS user_notifications;
 DROP TABLE IF EXISTS phone_comments;
 DROP TABLE IF EXISTS phone_follows;
