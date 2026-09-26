1220f1dc28bdb0210fe78bce657fb90d49cd80c4
wuangmmo <wuangmmo@gmail.com>
2026-09-26 11:30:51 +0700

    V44.2: add database/migrations/000023_control_center_foundation.down.sql

---
+++ database/migrations/000023_control_center_foundation.down.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS control_audit_logs;
+DROP TABLE IF EXISTS control_admin_assignments;
+DROP TABLE IF EXISTS control_role_permissions;
+DROP TABLE IF EXISTS control_roles;
+DROP TABLE IF EXISTS control_permissions;
+DROP TABLE IF EXISTS sites;
+DROP TABLE IF EXISTS organizations;
